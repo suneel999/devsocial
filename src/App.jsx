d35a4422c1b6fa779f9b032d422c16e1632fd159
Suneel999 <suneelraju999@gmail.com>
2025-12-30 16:11:18 +0530

    Fix import casing for Linux build

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Body from "./body";
+import Body from "./Body";
 import Login from "./Login";
 import Profile from "./Profile";
 import { Provider } from "react-redux";
